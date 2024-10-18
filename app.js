4de6d2b423fc404beece33c1d5fa8fa75c2f8ced
Amanda <agmarquezd@gmail.com>
2024-10-18 13:07:37 -0400

    subida

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require('express');
-const app = express ('app');
-require ('dotenv').config();
-// const contactRouter = require('./controllers/contact');
-const {MONGO_URI} = require("./config");
+const app = express('app');
+require('dotenv').config();
 const expressStaticGzip = require('express-static-gzip');
-const path = require ('path');
-
+const path = require('path');
 
 
 //rutas backend
 app.use(express.json())
-// app.use('/api/contact', contactRouter);
-
 app.use(expressStaticGzip(path.join(__dirname, 'dist')));
-app.get('/*', function(request, response) {
+app.get('/*', function (request, response) {
     response.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
 
--- config.js
+++ config.js
@@ -1,9 +1,3 @@
-const PAGE_URL = process.env.NODE_ENV === 'production' 
-? 'placeholder' 
-: 'http://localhost:3005';
-
-const MONGO_URI= process.env.NODE_ENV === 'production'
-? process.env.MONGO_URI_PROD
-: process.env.MONGO_URI_TEST
-
-module.exports = {PAGE_URL, MONGO_URI};
+const PAGE_URL = process.env.NODE_ENV === 'production'
+  ? 'placeholder'
+  : 'http://localhost:3005';
